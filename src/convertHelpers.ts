9f3f015ba41afe00f5ae9a4322fd3a919dfe664d
Stephen Davies <steve9164@gmail.com>
2021-03-04 01:53:40 +1100

    Update catalog-converter version and accept JSON5 catalogs

--- src/convertHelpers.ts
+++ src/convertHelpers.ts
@@ -1,4 +1,5 @@
 import { convertCatalog } from "catalog-converter";
+import json5 from "json5";
 
 export const parseCatalog = (catalog: any) => {
   const converted = convertCatalog(catalog);
@@ -9,7 +10,7 @@ export const parseCatalog = (catalog: any) => {
 export const parseFile = async (file: File) => {
   const string = await file.text();
   try {
-    const catalog = JSON.parse(string);
+    const catalog = json5.parse(string);
     return parseCatalog(catalog);
   } catch (e) {
     throw new Error(
@@ -21,18 +22,19 @@ export const parseFile = async (file: File) => {
 export const parseWeblink = (link: string) => {
   return fetch(link, {
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   })
-    .then(res => res.json())
-    .catch(err => {
+    .then((res) => res.text())
+    .then((string) => json5.parse(string))
+    .catch((err) => {
       const msg = `Had a problem with accessing the JSON at "${link}".
       Check that it is a valid link, or if you are sure it is valid,
       it may not be CORS enabled - try downloading the file to your computer
       and then dragging it onto this tool. ${err.message}`;
       throw new Error(msg);
     })
-    .then(json => {
+    .then((json) => {
       const newCatalog = parseCatalog(json);
       return newCatalog;
     });
